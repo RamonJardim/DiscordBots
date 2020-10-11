d2895eeb06acd4919aa5f7a8de285c163f04b5e1
Igor Iwashita de Oliveira <igor.iwashita@aluno.ufabc.edu.br>
2020-10-11 02:37:08 -0300

    Add user para ser kikado

--- nodeJS/router/discordActionController.js
+++ nodeJS/router/discordActionController.js
@@ -4,11 +4,12 @@ const constants = require('../util/constants');
 const functions = require('../util/functions');
 
 //Expulsa usuário do discord
-router.get("/kick", async (req, res, next) => {
+router.get("/kick/:user", async (req, res, next) => {
 
     const body = req.body;
+    const user = req.params.user;
 
-    functions.moveMemberToChannel(req.client, constants.yeyID, constants.yuruiID, constants.theMaciotaID).then((results)=>{
+    functions.moveMemberToChannel(req.client, constants.yeyID, constants[user], constants.theMaciotaID).then((results)=>{
         res.send({success: true, msg:"O Yurui foi expulso com sucesso"})
     }).catch((fail)=>{
         console.log(fail);
